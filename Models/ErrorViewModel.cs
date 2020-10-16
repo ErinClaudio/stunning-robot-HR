70f0dbc7bde0088fa81941de230d2af4d2ce31ae
Erin Claudio <eclaudio@gmail.com>
2020-10-16 10:19:47 -0400

    First real commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace stunning_robot_HR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
