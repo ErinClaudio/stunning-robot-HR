9cd0e51c6295afd9a943618780dac4968357d62a
Erin Claudio <eclaudio@gmail.com>
2020-10-23 09:51:27 -0400

    corrected staff class to use DateTime Property versus previous

--- Models/Staff.cs
+++ Models/Staff.cs
@@ -1,19 +1,18 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace stunning_robot_HR.Models
 {
     public class Staff
     {
-        public int Id { get; set; }
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-        
-        [Display(Name = "Total Hours Worked")]
-        public int TotalHoursWorked { get; set; }
-        
-        [Display(Name = "Total Num Of Available Days Off")]
-        public int NumberOfAvailableDaysOff { get; set; }
+        [Display(Name = "Full nfame")]
+        public DateTime FullName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+        public string Position { get; set; }
+
+        [Display(Name = "Start Date")]
+        public int StartDate { get; set; }
     }
 }
