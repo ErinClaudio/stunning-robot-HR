49c6e9a2ecfe9e0afc876d9d2f93d4f23dbc724c
Erin Claudio <eclaudio@gmail.com>
2020-11-25 10:09:12 -0500

    changing name of method

--- Controllers/SearchForStaff.cs
+++ Controllers/SearchForStaff.cs
@@ -12,11 +12,14 @@ namespace stunning_robot_HR.Controllers
     {
         private stunning_robot_HRContext _context;
         private IStaffRepository staffRepository;
-        public async Task<IActionResult> searchresults(string searchString)
+
+        //staffRepository.SearchStaff(searchString)
+        public async Task<IActionResult> GetStaffByFullName(string searchString)//are you recommending this so that this method is associated with correct controller?
         {
             var staff = staffRepository.GetStaffByFullName(searchString);
             return View(staff);
         }
-
+// I made the change to this method name per this comment
+//https://github.com/ErinClaudio/stunning-robot-HR/pull/38/files/9a7a57255602e898f1b90a8ecaceea78b6e3d330..d4341b419ea872b1e65f0d1340ac79e37838d19f#r528364940
     }
 }
