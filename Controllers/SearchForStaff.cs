fb00aaea917b10dc79733878d2fa0590c19a5f72
Erin Claudio <eclaudio@gmail.com>
2020-12-02 13:11:00 -0500

    having issues with the search feature using the correct model

--- Controllers/SearchForStaff.cs
+++ Controllers/SearchForStaff.cs
@@ -13,19 +13,35 @@ namespace stunning_robot_HR.Controllers
         private stunning_robot_HRContext _context;
         private IStaffRepository staffRepository;
         
-        public SearchForStaff(stunning_robot_HRContext context )
+        public SearchForStaff(stunning_robot_HRContext context)
         {
             _context = context;
             staffRepository = new StaffRepository(_context);
         }
-        public async Task<IActionResult> GetStaffByFullName(string searchString)//are you recommending this so that this method is associated with correct controller?
+
+        public async Task<IActionResult> GetStaffByFullName(string searchString)
         {
             var staff = staffRepository.GetStaffByFullName(searchString);
-            return View(staff);
+            return View(staff); // is this issue here in my return
         }
         
         
-// I made the change to this method name per this comment
-//https://github.com/ErinClaudio/stunning-robot-HR/pull/38/files/9a7a57255602e898f1b90a8ecaceea78b6e3d330..d4341b419ea872b1e65f0d1340ac79e37838d19f#r528364940
+        /*
+        public ViewResult GetStaffByFullName(string searchString)
+        {
+            Staff staff = staffRepository.GetStaffByFullName(searchString);
+            return View(staff); 
+        }
+        */
+        
+        /*
+        public ViewResult Details(int id)
+        {
+            Staff staff = staffRepository.GetStaffByID(id);
+            return View(staff);
+        }
+        */
+
     }
 }
+
